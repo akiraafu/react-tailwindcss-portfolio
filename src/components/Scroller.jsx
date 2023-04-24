88360cf9a1b7ee3d60a26e8a14784412fab3648b
Junrong Fu <junrongfu6@gmail.com>
2023-04-25 00:23:39 +0800

    set up homepage

---
+++ src/components/Scroller.jsx
@@ -0,0 +1,20 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const Scroller = ({ text, link }) => {
+  return (
+    <section>
+      <Link to={link}>
+        <Marquee
+          pauseOnHover
+          gradient={false}
+          className="bg-yellow-300 border-t-2 border-black py-2 font-bold text-4xl"
+        >
+          {text}
+        </Marquee>
+      </Link>
+    </section>
+  );
+};
+
+export default Scroller;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import { Portfolio1, Portfolio2, Portfolio3, Portfolio4 } from "./assets";
+
+export const portfolioData = [
+  {
+    title: "Akira's Brand",
+    thumnail: Portfolio1,
+    alt: "branding",
+    category: "branding",
+    link: "#",
+  },
+  {
+    title: "Akira's Brand",
+    thumnail: Portfolio2,
+    alt: "branding",
+    category: "branding",
+    link: "#",
+  },
+  {
+    title: "Akira's Brand",
+    thumnail: Portfolio3,
+    alt: "branding",
+    category: "branding",
+    link: "#",
+  },
+  {
+    title: "Akira's Brand",
+    thumnail: Portfolio4,
+    alt: "branding",
+    category: "branding",
+    link: "#",
+  },
+];
