17ba17d9aa03ec0919cf538abfa3848fc7f712b5
Junrong Fu <junrongfu6@gmail.com>
2023-04-24 22:04:46 +0800

    set up routes and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,64 @@
+import { Link, NavLink } from "react-router-dom";
+import { Logo } from "../assets/index";
+
+const activeClassName =
+  "relative before:content-['*'] before:-top-4 before:left-1/2 before:-ml-1 before:absolute before:text-amber-400";
+
+const Header = () => {
+  return (
+    <header className="grid grid-cols-2 md:grid-cols-[180px_1fr_180px] border-black border-2 bg-white">
+      <Link
+        to="/"
+        className="flex justify-center py-4 logoLink border-black border-r-2"
+      >
+        <img className="logo" src={Logo} alt="Akira" height="15" />
+      </Link>
+      <Link
+        to="/contact"
+        className="md:col-start-3 border-black md:border-l-2 flex gap-2 items-center justify-center text-lg bg-yellow-300 hover:bg-yellow-500 ease-in-out duration-75"
+      >
+        <i class="bx bx-right-arrow-alt"></i>
+        <span> Let's Talk</span>
+      </Link>
+      <nav className="col-span-2 md:row-start-1 md:col-start-2 md:col-end-3 border-t-2 border-black md:border-t-0 flex items-center justify-center">
+        <ul className="flex gap-6 max-w-sm overflow-auto snap-always p-4 text-lg">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink to="/about">Work</NavLink>
+          </li> */}
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
